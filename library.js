1852543c3f66b16713fa7d89f64e09b7a23fd633
hafizbukhari21 <hafiz.bukhari@hotmail.com>
2023-04-06 12:50:37 +0700

    transacionDataDetailLib

--- library.js
+++ library.js
@@ -77,4 +77,37 @@ let titleRecon = [
 ]
 
 
+let title_Transaction_data_detail = [
+    "MTI|N-4|",
+    "Processing Code|AN-6|",
+    "Response Code|N-2|Iso-field 39",
+    "Approval Code|AN-6|Iso-field 38",
+    "PAN|AN-19|Iso-field 35",
+    "Switch Receiving Date|yymmdd|",
+    "Switching Receiving Time|Hhmm24ss|",
+    "System Trace Audit Number|N-12|Iso-field 11",
+    "RRN|AN-12|Iso-field 37",
+    "Acq Par ID|AN-3|",
+    "Iss Par ID|AN-3|",
+    "Card Acceptor Name & Location|ANS-40|Iso-field 43",
+    "Acq Institution Indentification|N-11|Iso-field 32",
+    "Local Trx Date|yymmdd|Iso-field 13",
+    "Local Trx Time|HHmm24ss|Iso-field 12",
+    "Currency Code|N-3|Iso-field 49",
+    "Trx Amount|N..12|Iso-field 4",
+    "Card Acc Terminal Identity|AN-16|Iso-field 41",
+    "Switching Bussiness Date|yymmdd|",
+    "Internal Flag|N..2|",
+    "Channel Type|AN-4|",
+    "Destination Participant ID|AN..3|",
+    "Destination Account Number|AN..28|",
+    "Destination Institution Indetification Code|N..11|",
+    "Admin Fee|AN..16|Iso-field 48",
+    "Biller Amount|N..12|Biller Amount = Trx Amount-Admin Fee",
+    "Principal Code|N-3|"
+
+]
+
+
+
 
